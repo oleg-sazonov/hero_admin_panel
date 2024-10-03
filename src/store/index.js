b8bad6466e71e13a0a9c5512ab13867a9d573ca7
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-10-03 15:18:19 +0300

    feat(store, actions, HeroesFilters/HeroesList.js): add middlewares; add fetch-actionCreators

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,21 @@
-import { legacy_createStore as createStore, combineReducers } from 'redux';
+import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const store = createStore(combineReducers({heroes, filters}), 
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const stringMiddleware = () => (next) => (action) => {
+	if (typeof action === 'string') {
+		return next({
+			type: action
+		})
+	}
+	return next(action);
+}
+
+const store = createStore(
+					combineReducers({heroes, filters}),
+					compose(applyMiddleware(thunk, stringMiddleware),
+							window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+					);
 
 export default store;
